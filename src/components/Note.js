8066234ea758621f2ca326a8372fed76a2b45d58
woailvxy <xy17760536771@163.com>
2020-09-29 13:59:16 +0800

    自定义富文本

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,4 +1,11 @@
 import React from 'react';
 import RichEditorExample from "./RichEditorExample";
+import LinkEditorExample from "./LinkEditorExample";
+import ColorControls from "./ColorfulEditorExample";
+import HTMLConvertExample from "./HTMLConvertExample";
+import EntityEditorExample from "./EntityEditorExample";
+import MediaEditorExample from "./MediaEditorExample";
+import TweetEditorExample from "./TweetEditorExample";
+import IframedEditorExample from "./IframedEditorExample";
 
-export default RichEditorExample
+export default IframedEditorExample
