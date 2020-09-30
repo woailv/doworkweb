0c6c8c998eae43d628e0847adb7c298a44bca8c5
woailvxy <xy17760536771@163.com>
2020-09-30 17:38:16 +0800

    笔记

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,7 +1,12 @@
 import React, {Component} from 'react';
 
 // export Note
+// catalogue 目录
 class Note extends Component {
+    constructor(props) {
+        super(props);
+    }
+
     render() {
         return (
             <div>
